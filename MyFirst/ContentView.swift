de834e2d5e6c8bc5fc659ad95d6e133da4431af4
Sri Satish M <surendramavuri@gmail.com>
2024-03-15 22:46:29 -0400

    Initial Commit

---
+++ MyFirst/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyFirst
+//
+//  Created by Sri Satish M on 3/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
