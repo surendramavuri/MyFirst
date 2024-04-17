6ab849190d2304fa780b222af9a3bace2cd77e23
Sri Satish M <surendramavuri@gmail.com>
2024-04-17 11:45:07 -0400

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Sri Satish M on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
